5b5d797cc112d079aff2b20bf20e0854d6a03521
Henrique Possatto <henrique@pc-henrique.localdomain>
2019-08-15 03:12:56 -0300

    tá indo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Nav from './components/nav'
+import Home from './pages/home'
+import Contato from './pages/contato'
+import Fotos from './pages/fotos'
+import Equipe from './pages/equipe'
+import Pesquisa from './pages/pesquisa'
+import Teses from './pages/teses'
+import Publicacoes from './pages/publicacoes'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	state = {
+		en: false
+	}
+
+	componentDidMount() {
+		var opt = localStorage.getItem('en') || false
+
+		if (opt === 'true') {
+			document.title = 'Research Group - Gepecom'
+			this.setState({ en: true })
+		} else {
+			document.title = 'Grupo de Pesquisa - Gepecom'
+			this.setState({ en: false })
+		}
+	}
+
+	lang = () => {
+		if (this.state.en) {
+			this.setState({ en: false })
+			localStorage.setItem('en', false)
+			document.title = 'Grupo de Pesquisa - Gepecom'
+		} else {
+			this.setState({ en: true })
+			localStorage.setItem('en', true)
+			document.title = 'Research Group - Gepecom'
+		}
+	}
+
+	render() {
+		return (
+			<Router>
+				<div>
+					<Nav func={this.lang} en={this.state.en} />
+
+					<Route exact path="/" render={() => <Home en={this.state.en} />} />
+
+					<Route path="/equipe" render={() => <Equipe en={this.state.en} />} />
+					<Route
+						path="/pesquisa"
+						render={() => <Pesquisa en={this.state.en} />}
+					/>
+					<Route
+						path="/publicacoes"
+						render={() => <Publicacoes en={this.state.en} />}
+					/>
+					<Route path="/teses" render={() => <Teses en={this.state.en} />} />
+					<Route path="/fotos" render={() => <Fotos en={this.state.en} />} />
+					<Route
+						path="/contato"
+						render={() => <Contato en={this.state.en} />}
+					/>
+					<footer style={{ marginTop: 20, textAlign: 'center' }}>
+						<p>
+							<img
+								src={require('./imagens/imagem_unifeilogo.jpg')}
+								style={{ height: '50px', marginRight: 10 }}
+								alt="logo"
+							/>
+							{this.state.en
+								? 'Federal University of Itajuba'
+								: 'Universidade Federal de Itajubá'}
+						</p>
+					</footer>
+				</div>
+			</Router>
+		)
+	}
 }
 
-export default App;
+export default App
