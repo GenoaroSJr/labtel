6f0387e30065fea17c17ada32ebd3c34831a5108
Henrique Possatto <henrique@pc-henrique.localdomain>
2019-08-15 23:49:10 -0300

    ver 1

--- src/pages/fotos.js
+++ src/pages/fotos.js
@@ -1,156 +1,32 @@
 import React from 'react'
-import { Container, Row, Col, Card, Image } from 'react-bootstrap'
+import { Container, Card, Image } from 'react-bootstrap'
 
-const Fotos = ({ en }) => (
-	<Container style={{ marginTop: 20 }}>
-		<h1>{en ? 'Photo Gallery' : 'Galeria de Fotos'}</h1>
-		<Card />
+class Fotos extends React.Component {
+	importAll(r) {
+		return r.keys().map(r)
+	}
 
-		<Row style={{ marginTop: 15 }}>
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria32.jpg')} />
-			</Col>
+	func = img => (
+		<Image
+			src={img}
+			style={{ width: '30%', marginBottom: 20, marginRight: 10 }}
+			key={img}
+		/>
+	)
 
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria2.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria3.jpg')} />
-			</Col>
-		</Row>
-
-		<Row style={{ marginTop: 15 }}>
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria5.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria6.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria7.jpg')} />
-			</Col>
-		</Row>
-
-		<Row style={{ marginTop: 15 }}>
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria8.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/EFEI_logo.png')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria9.jpg')} />
-			</Col>
-		</Row>
-
-		<Row style={{ marginTop: 15 }}>
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria10.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria12.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria13.jpg')} />
-			</Col>
-		</Row>
-
-		<Row style={{ marginTop: 15 }}>
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria14.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria15.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria16.jpg')} />
-			</Col>
-		</Row>
-
-		<Row style={{ marginTop: 15 }}>
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria17.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria18.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria19.jpg')} />
-			</Col>
-		</Row>
-
-		<Row style={{ marginTop: 15 }}>
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria20.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria21.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria22.jpg')} />
-			</Col>
-		</Row>
-
-		<Row style={{ marginTop: 15 }}>
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria23.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria24.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria25.jpg')} />
-			</Col>
-		</Row>
-
-		<Row style={{ marginTop: 15 }}>
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria26.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria27.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria28.jpg')} />
-			</Col>
-		</Row>
-
-		<Row style={{ marginTop: 15 }}>
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria29.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria30.jpg')} />
-			</Col>
-
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria31.jpg')} />
-			</Col>
-		</Row>
-		<Row style={{ marginTop: 15 }}>
-			<Col md={4} style={{ marginTop: 8 }}>
-				<Image fluid src={require('../imagens/galeria/imagemGaleria1.jpg')} />
-			</Col>
-		</Row>
-	</Container>
-)
+	render() {
+		const images = this.importAll(
+			require.context('../imagens/galeria', false, /\.(png|jpe?g|svg)$/)
+		)
+		images.reverse()
+		return (
+			<Container style={{ marginTop: 20 }}>
+				<h1>{this.props.en ? 'Photo Gallery' : 'Galeria de Fotos'}</h1>
+				<Card style={{ marginBottom: 30 }} />
+				{images.map(this.func)}
+			</Container>
+		)
+	}
+}
 
 export default Fotos
