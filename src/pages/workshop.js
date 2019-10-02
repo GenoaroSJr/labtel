b5a388a31046cd79aa35cc58e73b208591e68883
Henrique Marcelino Possatto <henriquempossatto@gmail.com>
2019-10-01 23:16:38 -0300

    aaa

--- src/pages/workshop.js
+++ src/pages/workshop.js
@@ -1,27 +1,41 @@
 import React from "react";
-import { Container, Card, Image, Row, Col } from "react-bootstrap";
+import { Container, Card, Image, Row, Col, Button } from "react-bootstrap";
 import Iframe from "react-iframe";
 
 class Workshop extends React.Component {
   render() {
     return (
-      <Container fluid style={{ marginTop: 20, padding: 20 }}>
+      <Container fluid style={{ marginTop: 20, padding: 20, width: "80%" }}>
         <h1>Workshop</h1>
         <Card style={{ marginBottom: 30 }} />
 
         <Row>
           <Col style={{ textAlign: "right" }}>
             <Image
-              src={require("../imagens/LAIoT/programa1.png")}
-              style={{ width: "70%" }}
+              src={require("../imagens/LAIoT/handson.png")}
+              style={{ width: "100%" }}
             />
           </Col>
 
           <Col style={{ textAlign: "left" }}>
             <Image
-              src={require("../imagens/LAIoT/handson.png")}
-              style={{ width: "65%" }}
+              src={require("../imagens/LAIoT/programa1.png")}
+              style={{ width: "100%" }}
             />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col style={{ textAlign: "left", marginTop: 20, marginBottom: 30 }}>
+            <Button
+              style={{ fontSize: 50 }}
+              variant="primary"
+              size="lg"
+              block
+              href="https://docs.google.com/forms/d/e/1FAIpQLSccPU2mhvXc_MRS0gCopEJxmjhZbyHrofaHMCgIejSwrADqRw/viewform?usp=sf_link"
+            >
+              Inscreva-se!
+            </Button>
           </Col>
         </Row>
 
@@ -29,14 +43,7 @@ class Workshop extends React.Component {
           <Col style={{ textAlign: "center" }}>
             <Image
               src={require("../imagens/LAIoT/mapa.png")}
-              style={{ width: "50%" }}
-            />
-          </Col>
-
-          <Col style={{ textAlign: "left" }}>
-            <Image
-              src={require("../imagens/LAIoT/programa2.png")}
-              style={{ width: "70%" }}
+              style={{ width: "30%" }}
             />
           </Col>
         </Row>
@@ -56,12 +63,12 @@ class Workshop extends React.Component {
         <h2>Dashboard</h2>
         <Card style={{ marginBottom: 30 }} />
         <Row>
-          <Col style={{ marginLeft: "18%" }}>
+          <Col>
             <Iframe
               url="https://admin.tago.io/public/dashboard/5d6d1a0eca5003001b180a54/cb4092f0-a1f2-47e1-a329-e1ae13bf4b23"
-              height="900px"
+              height="650px"
               scrolling="no"
-              width="80%"
+              width="100%"
             />
           </Col>
         </Row>
