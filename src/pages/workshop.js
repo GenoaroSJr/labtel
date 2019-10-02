943426d61d95d8c27bd635dbde75aae231120ca5
Henrique Marcelino Possatto <henriquempossatto@gmail.com>
2019-10-02 10:44:48 -0300

    workshop 2.0

--- src/pages/workshop.js
+++ src/pages/workshop.js
@@ -5,21 +5,26 @@ import Iframe from "react-iframe";
 class Workshop extends React.Component {
   render() {
     return (
-      <Container fluid style={{ marginTop: 20, padding: 20, width: "80%" }}>
+      <Container fluid style={{ marginTop: 20, padding: 20, width: "90%" }}>
         <h1>Workshop</h1>
         <Card style={{ marginBottom: 30 }} />
 
         <Row>
-          <Col style={{ textAlign: "right" }}>
+          <Col sm="4" style={{ textAlign: "right" }}>
             <Image
-              src={require("../imagens/LAIoT/handson.png")}
+              src={require("../imagens/LAIoT/handson.jpg")}
               style={{ width: "100%" }}
             />
           </Col>
-
-          <Col style={{ textAlign: "left" }}>
+          <Col sm="4" style={{ textAlign: "center" }}>
             <Image
-              src={require("../imagens/LAIoT/programa1.png")}
+              src={require("../imagens/LAIoT/pint.jpg")}
+              style={{ width: "100%" }}
+            />
+          </Col>
+          <Col sm="4" style={{ textAlign: "left" }}>
+            <Image
+              src={require("../imagens/LAIoT/sexta.jpg")}
               style={{ width: "100%" }}
             />
           </Col>
@@ -29,9 +34,9 @@ class Workshop extends React.Component {
           <Col style={{ textAlign: "left", marginTop: 20, marginBottom: 30 }}>
             <Button
               style={{ fontSize: 50 }}
-              variant="primary"
               size="lg"
               block
+              variant="danger"
               href="https://docs.google.com/forms/d/e/1FAIpQLSccPU2mhvXc_MRS0gCopEJxmjhZbyHrofaHMCgIejSwrADqRw/viewform?usp=sf_link"
             >
               Inscreva-se!
@@ -40,16 +45,23 @@ class Workshop extends React.Component {
         </Row>
 
         <Row>
-          <Col style={{ textAlign: "center" }}>
-            <Image
-              src={require("../imagens/LAIoT/mapa.png")}
-              style={{ width: "30%" }}
+          <Col sm="10">
+            <h2>Dashboard</h2>
+            <Card style={{ marginBottom: 30 }} />
+            <Iframe
+              url="https://admin.tago.io/public/dashboard/5d6d1a0eca5003001b180a54/cb4092f0-a1f2-47e1-a329-e1ae13bf4b23"
+              height="650px"
+              scrolling="no"
+              width="100%"
             />
           </Col>
-        </Row>
-
-        <Row>
-          <Col style={{ textAlign: "center" }}>
+          <Col sm="2">
+            <h2>Mapa</h2>
+            <Card style={{ marginBottom: 30 }} />
+            <Image
+              src={require("../imagens/LAIoT/mapa.png")}
+              style={{ width: "100%" }}
+            />
             <p>
               Para maiores informações um mapa completo pode da Unifei pode ser
               conferido no link{" "}
@@ -57,19 +69,6 @@ class Workshop extends React.Component {
                 Mapa da Unifei
               </a>
             </p>
-          </Col>
-        </Row>
-
-        <h2>Dashboard</h2>
-        <Card style={{ marginBottom: 30 }} />
-        <Row>
-          <Col>
-            <Iframe
-              url="https://admin.tago.io/public/dashboard/5d6d1a0eca5003001b180a54/cb4092f0-a1f2-47e1-a329-e1ae13bf4b23"
-              height="650px"
-              scrolling="no"
-              width="100%"
-            />
           </Col>
         </Row>
         <Row>
